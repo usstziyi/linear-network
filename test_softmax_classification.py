c2506e902ef3e75bb9a77b2cef166f0dfd86eb9f
007 <007lookworld@gmail.com>
2025-10-03 21:48:07 +0800

    perf(训练计时): 添加计时功能并改进训练日志输出
    在训练循环中添加计时器以测量每轮训练时间，并改进日志输出格式显示当前轮次和总轮次

--- test_softmax_classification.py
+++ test_softmax_classification.py
@@ -6,6 +6,8 @@ from torchvision import transforms
 from d2l import torch as d2l
 from common import get_dataloader_workers
 from classification import SoftmaxClassification
+from common import Timer
+
 
 
 # 加载Fashion-MNIST数据集
@@ -38,10 +40,12 @@ def train_SoftMaxClassification(model, train_iter, test_iter, num_epochs=10):
     loss = nn.CrossEntropyLoss(reduction='none')
     # 定义优化器：随机梯度下降（SGD）优化器
     optimizer = torch.optim.SGD(model.parameters(), lr=0.1)
-
+    # 初始化计时器
+    timer = Timer()
 
     # 训练模型
     for epoch in range(num_epochs):
+        timer.start()  # 开始计时
         # 训练模型并计算训练损失和训练准确率
         train_metrics = d2l.Accumulator(3)
         model.train()
@@ -62,10 +66,12 @@ def train_SoftMaxClassification(model, train_iter, test_iter, num_epochs=10):
                 l = loss(y_hat, y) # 内部会自动对 logits 应用 softmax 并计算交叉熵损失
                 test_metrics.add(l.sum(), d2l.accuracy(y_hat, y), y.numel())
             test_loss, test_acc, test_samples = test_metrics
-            print(f"epoch {epoch+1}: train loss {train_loss/train_samples:.4f}, "
+            timer.stop()  # 停止计时
+            print(f"epoch {(epoch+1):2d}/{num_epochs}: train loss {train_loss/train_samples:.4f}, "
                   f"train acc {train_acc/train_samples:.4f}, "
                   f"test loss {test_loss/test_samples:.4f}, "
-                  f"test acc {test_acc/test_samples:.4f}")
+                  f"test acc {test_acc/test_samples:.4f}, "
+                  f"time {timer.sum():.2f} sec")
     
 
 def main():
